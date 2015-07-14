35b5917898d29beea8aafca4d259bb18a070a1a8
sandmakers <onionsbm6@gmail.com>
2015-07-14 20:12:10 +0800

    no message

---
+++ online/chat.js
@@ -0,0 +1,12 @@
+document.writeln("<script language=\"javascript\" src=\"http://pkt.zoosnet.net/JS/LsJS.aspx?siteid=PKT21646611&float=1&lng=en\"></script>");
+
+
+function Online_Chat(){
+	window.open('http://pkt.zoosnet.net/LR/Chatpre.aspx?id=PKT21646611&lng=en');
+}
+
+
+
+
+
+
